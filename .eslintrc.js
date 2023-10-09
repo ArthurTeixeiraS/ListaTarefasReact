6fc78ccbdce00115e0de3bf023d9b0c3dbd6cdb3
Arthur Teixeira Serafim <arthurteixeiraserafim@gmail.com>
2023-10-08 22:11:59 -0300

    Lista de tarefas com React!

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: 'airbnb',
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [
+        '.eslintrc.{js,cjs}',
+      ],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parser: '@babel/eslint-parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    'react/jsx-filename-extension': 0,
+    requireConfigFile: 0,
+    'react/state-in-constructor': 0,
+  },
+};
